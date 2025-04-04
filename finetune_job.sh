28c4cf5be1adfe307bcbf2209e50977b4b74c73f
guandong <gn533549@dal.ca>
2025-04-04 02:39:10 -0300

    try

--- finetune_job.sh
+++ finetune_job.sh
@@ -3,11 +3,15 @@
 #SBATCH --output=output.log         # 标准输出文件
 #SBATCH --error=error.log           # 错误输出文件
 #SBATCH --ntasks=1                  # 任务数
-#SBATCH --time=02:00:00             # 运行时间上限
-#SBATCH --gres=gpu:1                # 请求 GPU 数量
+#SBATCH --time=04:00:00             # 运行时间上限
+#SBATCH --gres=gpu:4                # 请求 GPU 数量
 
 # 加载必要模块
 module load python/3.12
+module load gcc
+module load arrow/19
+source /project/6078835/gn533549/LLMPersonality/ENV/bin/activate
+pip install pyarrow --no-index
 pip install torch --no-index
 
 # 执行你的 AI 程序
