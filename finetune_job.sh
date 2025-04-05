92254d35c63a592151b0475ec9985c98e982a88a
guandong <gn533549@dal.ca>
2025-04-04 22:39:50 -0300

    lager mem

--- finetune_job.sh
+++ finetune_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=error_%a.log
 #SBATCH --ntasks=1
 #SBATCH --time=08:00:00
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:4
 #SBATCH --array=0-2
